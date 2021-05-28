bce528a9ab2ee42a9fb662c2d1c83db8a7b51221
EmreKayaoglu <emrefurkankayao@gmail.com>
2021-05-28 18:03:51 +0200

    add response

--- app.js
+++ app.js
@@ -23,6 +23,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(serveStatic(__dirname + '/public'));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  if (req.method === "OPTIONS") {
+      return res.status(200).end();
+  }
+  
+  next();
+});
+
 // Routes
 
 app.get('/', routes.index);
