50e2b03c4957948d3f7570c408599247f9ff6d0d
EmreKayaoglu <emrefurkankayao@gmail.com>
2021-05-28 17:51:48 +0200

    add response

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,27 @@ function getClientIp(req) {
  */
 
 exports.index = function(req, res) {
-  res.end('Hello World');  
+  res.end(`[{
+    "slug":"we-conect",
+    "title":"We-Conect Home page",
+    "url":"https://www.we-conect.com/",
+    "children": [
+    {
+    " slug ":"live-events",
+    "title":"Live Event",
+    "url":"https://www.we-conect.com/liveevents",
+    } ,
+    {
+    " slug ":"digital-managed-events ",
+    "title":"Digital Managed Events ",
+    "url":"https://www.we-conect.com/l digital-managed-events ",
+    } 
+    ]
+   },
+    {
+    " slug ":"google",
+    "title":"Google",
+    "url":"https://www.google.de"
+   }
+   ]`);
 };
